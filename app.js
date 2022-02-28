3bf9021cb4208f67961075fd6b0655ddc9e1f56b
zonglinlee <tumble_weed@icloud.com>
2022-02-28 11:56:40 +0800

    1 file modified

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-import { createSSRApp } from 'vue';
+import {createSSRApp} from 'vue';
 
 export function createApp() {
-  return createSSRApp({
-    data: () => ({ count: 1 }),
-    template: `<div @click="count++">{{ count }}</div>`,
-  });
+    return createSSRApp({
+        data: () => ({
+            count: 1,
+            total: 1,
+        }),
+        template: `
+          <div style="display: flex;">
+            <button  @click="count++">{{ count }}</button>
+            <button style="margin-left: 100px;width: 30px" @click="addTotal">{{ total }}</button>
+          </div>`,
+        methods: {
+            addTotal() {
+                this.total = this.count * 6
+            }
+        }
+    });
 }
